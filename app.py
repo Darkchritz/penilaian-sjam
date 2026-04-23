b18aa9ea0010a8f696268acf31d2dfce27208385
Darkchritz <w3ndywangsaharja@gmail.com>
2026-04-23 16:11:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -127,6 +127,7 @@ def logout():
     session.clear()
     return redirect('/')
 
+init_db()  # pindah ke luar biar kepanggil pas gunicorn start
+
 if __name__ == '__main__':
-    init_db()
     app.run(host='0.0.0.0', port=5000)
