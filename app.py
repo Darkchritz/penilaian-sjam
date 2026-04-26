fcd57154d3d56bc1c2dea99d8b2183069d416d84
Darkchritz <w3ndywangsaharja@gmail.com>
2026-04-27 01:25:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -532,6 +532,10 @@ def logout():
     session.clear()
     return redirect('/login')
 
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
