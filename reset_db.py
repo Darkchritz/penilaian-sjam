d11372294a75254fcdce6107410a1e4fd0d4c648
Darkchritz <w3ndywangsaharja@gmail.com>
2026-04-29 16:54:04 +0800

    Update reset_db.py

--- reset_db.py
+++ reset_db.py
@@ -1,10 +1,9 @@
-from app import app, db
-# Import semua model biar SQLAlchemy tau tabel apa aja yang harus di-create
-from app import Karyawan, Penilaian, AksesPenilaian 
+   from app import app, db
+   from app import Karyawan, Penilaian, AksesPenilaian 
 
-with app.app_context():
-    print("Drop semua tabel...")
-    db.drop_all()
-    print("Create semua tabel...")
-    db.create_all()
-    print("Selesai! Database udah di-reset.")
+   with app.app_context():
+       print("Drop semua tabel...")
+       db.drop_all()
+       print("Create semua tabel...")
+       db.create_all()
+       print("Selesai!")
